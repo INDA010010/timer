7ffd2eb9d3bdfc6561ffe0a716196e806a430ed4
INDA010010 <74112294+INDA010010@users.noreply.github.com>
2020-12-15 18:01:39 +0100

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,27 @@
+from time import sleep
+micro = 0
+sec = 0
+min = 0
+hour = 0
+day = 0
+while True:
+	sleep(0.1)
+	micro += 1
+	print('day: ',day,' hour: ',hour,' min: ',min,' sec: ',sec,' microsec: ',micro)
+	if micro == 10:
+		micro = 0
+		sec += 1
+		print('day: ',day,' hour: ',hour,' min: ',min,' sec: ',sec,' microsec: ',micro)
+	if sec == 60:
+		sec = 0
+		min += 1
+		print('day: ',day,' hour: ',hour,' min: ',min,' sec: ',sec,' microsec: ',micro)
+	if min == 60:
+		min = 0
+		hour += 1
+		print('day: ',day,' hour: ',hour,' min: ',min,' sec: ',sec,' microsec: ',micro)
+	if hour == 24:
+		hour = 0
+		day += 1
+		print('day: ',day,' hour: ',hour,' min: ',min,' sec: ',sec,' microsec: ',micro)
+
